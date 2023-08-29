38d9f6c2fe7af9218c7df6d75199a4d7eceb3aaa
Tyler Brooks <tylerjbrooks@yahoo.com>
2023-08-29 16:45:52 -0700

    Added the 'new-dirt' route for the new dirt pages/wizard

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/new-dirt', function() {
+    return view('new-dirt'); 
+})->name('new-dirt');
 
 Route::middleware([
     'auth:sanctum',

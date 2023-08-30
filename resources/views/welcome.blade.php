29a887f1d47333fa94d2fe53a396152c0e729585
Tyler Brooks <tylerjbrooks@yahoo.com>
2023-08-29 18:20:21 -0700

    Messing around with the new-dirt buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-landing-layout>
     <div class="flex justify-center my-6">
-        <div class="basis-2/5 mr-2 md:basis-1/3 md:mr-4">
+        <div class="basis-2/5 mr-2 md:basis-1/3 md:mr-6">
             <a href="{{route('new-dirt')}}">
                 <x-harmony.button class="w-full">I Have Dirt</x-harmony.button>
             </a>
         </div>
-        <div class="basis-2/5 mr-2 md:basis-1/3 md:mr-4">
+        <div class="basis-2/5 mr-2 md:basis-1/3 md:mr-6">
             <a href="{{route('new-dirt')}}">
                 <x-harmony.button class="w-full">I Need Dirt</x-harmony.button>
             </a>

836bf2e54b0b0b9a197c5d3972ae6ca46f83954b
Tyler Brooks <tylerjbrooks@yahoo.com>
2023-08-29 18:11:41 -0700

    Added route names

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 Route::get('/new-dirt', function() {
     return view('new-dirt'); 
 })->name('new-dirt');
